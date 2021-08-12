1e2d8dc3698bbba94ae2c933b5b14d2bc01c550d
Iman Faizal <imanfz1103@gmail.com>
2021-08-12 21:14:45 +0700

    initial commit

---
+++ RAWG/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RAWG
+//
+//  Created by Iman Faizal on 06/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
