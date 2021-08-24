d8bc9c3eefffcb0bbad3a1aa41b8f42f62064ebb
Iman Faizal <imanfz1103@gmail.com>
2021-08-24 15:01:36 +0700

    add favorite model and profile model

---
+++ RAWG/data/Model/ProfileModel.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileModel.swift
+//  RAWG
+//
+//  Created by Iman Faizal on 24/08/21.
+//
+
+import Foundation
+
+struct ProfileModel {
+    var photo: Any?
+    var name: String?
+    var email: String?
+}
