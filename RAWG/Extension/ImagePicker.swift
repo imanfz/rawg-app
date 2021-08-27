34bc73befd76b5243978af9d3877543a301e5857
Iman Faizal <55774481+imanfz@users.noreply.github.com>
2021-08-27 19:29:47 +0700

    fixing and add feat edit profile

---
+++ RAWG/Extension/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  RAWG
+//
+//  Created by Iman Faizal on 27/08/21.
+//
+
+import Foundation
---
+++ RAWG/Resources/RoundButton.swift
@@ -0,0 +1,8 @@
+//
+//  RoundButton.swift
+//  RAWG
+//
+//  Created by Iman Faizal on 27/08/21.
+//
+
+import Foundation
